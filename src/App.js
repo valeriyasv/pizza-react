8c210b1d97056dd71e0ab3e9878aa6a5a457e711
valeriyasv <lerushka.sav@gmail.com>
2023-12-22 22:21:42 -0600

    breaks code into components, adds styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+import Header from './components/header/Header';
+import Sort from './components/sort/Sort';
+import Categories from './components/categories/Categories';
+import PizzaItem from './components/pizza-item/PizzaItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaItem />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
