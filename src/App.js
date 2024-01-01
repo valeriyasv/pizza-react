0a4bef717d170de7d92f3461af7a55251bd2b209
valeriyasv <lerushka.sav@gmail.com>
2024-01-01 14:39:13 -0600

    get pizzas from the backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './scss/app.scss';
+import React from 'react';
 import Header from './components/header/Header';
 import Sort from './components/sort/Sort';
 import Categories from './components/categories/Categories';
 import PizzaItem from './components/pizza-item/PizzaItem';
-import pizzas from './assets/pizza.json';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+  
+  React.useEffect(() =>  {
+    fetch('https://6593198fbb12970719905dff.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setPizzas(arr)
+      })
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
